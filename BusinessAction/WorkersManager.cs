3c36d6eb28a76c735bdc4deba94dcf15c933b923
CORREA <betsabe.correa@tecnologicoliceocristiano.edu.ec>
2023-12-29 21:04:03 -0500

    Agregar archivos de proyecto.

---
+++ BusinessAction/WorkersManager.cs
@@ -0,0 +1,36 @@
+﻿using SoapServiceApplicatition.BusinessMessage;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SoapServiceApplicatition.BusinessAction
+{
+    public static class WorkersManager
+
+    {
+        public static WorkersResponse GetAllWorkers()
+        {
+           var workers = new  List<Person>() {
+               new Person()
+               
+               {
+                 FullName= "Roberto Carlo"
+               }               
+           };
+            {
+                new Person()
+                {
+                   FullName= "maria lopez"
+                };
+            }
+
+            return new WorkersResponse()
+            {
+                Workers = workers
+            };
+            
+        }
+
+    }
+}
---
+++ BusinessMessage/WorkersResponse.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SoapServiceApplicatition.BusinessMessage
+{
+    public class WorkersResponse
+    {
+        public List<Person> Workers{ get; set; }
+
+        public bool ActionResult { get; set; }
+        
+        public string Error { get; set; }
+
+    }
+}
